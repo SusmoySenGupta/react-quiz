8cb8b208d7afb0923a5d3acd17780f716b670133
SusmoySenGupta <susmoy.cse@gmail.com>
2023-04-30 20:01:10 +0600

    feat: add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
 import Result from './pages/Result';
-// import Quiz from './pages/Quiz';
-// import Login from './pages/auth/Login';
-// import SignUp from './pages/auth/SignUp';
-// import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import SignUp from './pages/auth/SignUp';
 import './styles/App.css';
 
 function App() {
 	return (
-		<Layout>
-			{/* <Home /> */}
-			{/* <SignUp /> */}
-			{/* <Login /> */}
-			{/* <Quiz /> */}
-			<Result />
-		</Layout>
+		<Router>
+			<Layout>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/sign-up" element={<SignUp />} />
+					<Route exact path="/quiz" element={<Quiz />} />
+					<Route exact path="/result" element={<Result />} />
+				</Routes>
+			</Layout>
+		</Router>
 	);
 }
 
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import classes from '../styles/ProgressBar.module.css';
 import Button from './ui/Button';
 
@@ -16,15 +17,14 @@ export default function ProgressBar() {
 					></div>
 				</div>
 			</div>
-			<a href="result.html">
+			<Link to="/result">
 				<Button className="next">
 					<span>Next Question</span>
 					<span className="material-icons-outlined">
-						{' '}
-						arrow_forward{' '}
+						arrow_forward
 					</span>
 				</Button>
-			</a>
+			</Link>
 		</div>
 	);
 }
